dc09cb5d5ec6a026923a6fd32f6ffddbf9a5c32e
Alex Roper <aroper@umich.edu>
2012-09-16 16:16:53 -0400

    Pad now uses Interval, and is closer to done than bofore.

--- justthisonce/pad.py
+++ justthisonce/pad.py
@@ -8,7 +8,8 @@ import cPickle
 import os
 import collections
 
-import invariant
+from justthisonce.interval import Interval
+from justthisonce import invariant
 
 COMPAT = 0
 VERSION = 0
@@ -17,7 +18,11 @@ class Error(Exception):
   """Base error for the pad module."""
 
 class InvalidPad(Error):
-  """The on-disk pad structure has broken invariants, is corrupt, etc."""
+  """The on-disk pad structure has broken invariants, is corrupt, etc, and
+     there is no obvious way to repair it."""
+
+class PadDirty(Error):
+  """The on-disk pad is in an odd state but we can probably fix it safely."""
 
 class OutOfPad(Error):
   """There is not enough pad remaining to allocate the requested range."""
@@ -30,99 +35,62 @@ class Allocation(object):
      multiple files. They may be constructed with union-like updates."""
   __metaclass__ = invariant.EnforceInvariant
 
-  def __init__(self, filename=None, start=None, length=None):
+  def __init__(self, padfile=None, start=None, length=None):
     """With no arguments, creates an empty allocation. With three, creates an
        allocation with a single interval."""
 
-    # Mapping from filename to (start, length) list
+    # Mapping from filename to (padfile, intervals)
     self._alloc = collections.OrderedDict()
     self._size = 0
 
-    if filename is None or start is None or length is None:
-      assert filename is None and start is None and length is None
+    if padfile is None or start is None or length is None:
+      assert padfile is None and start is None and length is None
     else:
-      self._alloc.setdefault(filename, []).append((start, length))
+      self._alloc[padfile.filename] = (Interval.fromAtom(start, length), \
+                                       padfile)
       self._size = length
 
   def _checkInvariant(self):
-    #TODO Consider somehow combining the overlap between Allocation and File,
-    #     they have some similar logic with the extents. Keeping seperate for
-    #     now since allocation holes have different semantic meaning and don't
-    #     know their total underlying size.
-    #
-    #     Maybe decouple the logic for an interval type from semantic meaning?
-    size = 0
-    for (filename, extents) in self._alloc.iteritems():
-      ptr = 0
-      for (start, length) in extents:
-        assert start >= ptr
-        assert length > 0
-        ptr = start + length
-        size += length
-    assert size == self._size
+    assert self._size == sum((len(ival) for ival in self._alloc.itervalues()))
 
   def __len__(self):
     return self._size
 
   def union_update(self, other):
-    """Combines two allocations, merging adjacent intervals. The allocations
-       must not overlap, as there is never a good reason for this even though
-       union is straightforward to compute either way. The result will have
-       my files first, merged with the other's as appropriate, followed by the
-       files just in the other."""
-    post_merge = collections.OrderedDict()
-    for (filename, extents) in self._alloc.iteritems():
+    rval = self.union(other)
+    self._alloc = rval._alloc
+    self._size = rval._size
+
+  def union(self, other):
+    """Combines two allocations, merging adjacent intervals in the same file.
+       The result will have my files first, merged with the other's as
+       appropriate, followed by the files just in the other."""
+    merged = collections.OrderedDict()
+    for (filename, (ival, padfile)) in self._alloc.iteritems():
       if filename in other._alloc:
-        # Need to merge intervals
-        self_i = other_i = 0
-
-        # The current open interval
-        start_cur = 0
-        length_cur = 0
-
-        # Walk all intervals in order of start.
-        while self_i < len(self._alloc[filename]) or \
-              other_i < len(other._alloc[filename]):
-          if self_i < len(self._alloc[filename]) and \
-             (other_i == len(self._alloc[filename]) or \
-             self._alloc[filename][self_i] < other._alloc[filename][other_i]):
-            start, length = self._alloc[filename]
-            self_i += 1
-          else:
-            start, length = other._alloc[filename]
-            other_i += 1
-
-          # Intervals may not overlap.
-          assert start >= start_cur + length_cur
-
-          if start == start_cur + length_cur:
-            # We can merge this with the current interval.
-            length_cur += length
-          else:
-            # There was a skip so we need to save the old current and start
-            # a new one.
-            post_merge[filename].append((start_cur, length_cur))
-            start_cur, length_cur = start, length
-
-        # Don't forget to push the final interval, if applicable
-        if length_cur > 0:
-          post_merge[filename].append((start_cur, length_cur))
-
+        other_ival, other_padfile = other._alloc[filename]
+        assert other_padfile is padfile
+        merged[filename] = (ival.union(other_ival), padfile)
       else:
-        post_merge[filename] = extents
+        post_merge[filename] = (ival, padfile)
 
-    for (filename, extents) in other._alloc.iteritems():
-      if filename not in self._alloc:
-        post_merge[filename] = extents
+    # Add in any files just in the other.
+    map(merged.setdefault, other._alloc.iteritems())
 
-    self._size = self._size + other._size
-    self._alloc = post_merge
+    rval = Allocation()
+    rval._size = self._size + other._size
+    rval._alloc = merged
+    return rval
+
+  def iterFiles(self):
+    """Returns an iterator of (interval, file)."""
+    return self._alloc.itervalues()
 
 class File(object):
   """Holds data on what parts of a file have already been used."""
   __metaclass__ = invariant.EnforceInvariant
 
-  def __init__(self, filename, size):
+  def __init__(self, filename, size, subdir):
     # Total size of the file
     self.size = size
 
@@ -132,21 +100,19 @@ class File(object):
     # Relative name of the file.
     self.filename = filename
 
-    # Regions of the file in use. Must be non-overlapping and sorted.
-    self._extents = []
+    # Regions of the file in use.
+    self._extents = Interval
+
+    # Subdir the file is currently in.
+    self._subdir = subdir
 
   def _checkInvariant(self):
     assert self.size >= self.used
-    # Yes, this is O(n). However remember that there will seldom, if ever, be
-    # more than a few extents.
-    ptr = 0
-    used = 0
-    for (start, length) in self._extents:
-      assert start >= ptr
-      assert length > 0
-      used += length
-      ptr = start + length
-    assert used == self.used
+    assert len(self._extents) == self.used
+
+  @property
+  def free(self):
+    return self.size - self.used
 
   def getAllocation(self, requested):
     """Requests an allocation from the file of the given size. Raises OutOfPad
@@ -155,27 +121,21 @@ class File(object):
       raise OutOfPad("File %i has %i bytes but you requested %i." % \
                      (self.filename, self.free, requested))
 
-    ptr = 0
     alloc = Allocation()
-    for (start, length) in self._extents:
-      if start > ptr:
-        seg = Allocation(self.filename, start, \
-                         min(requested - len(alloc), start - ptr))
-        alloc.union_update(seg)
-        ptr = start + length
-      if len(alloc) >= requested:
-        break
-    else:
-      seg = Allocation(self.filename, ptr, \
-                       min(requested - len(alloc), self.size - ptr))
+    for (start, length) in self._extents.iterExterior(self.size):
+      seg = Allocation(self, start, min(requested - len(alloc), length))
       alloc.union_update(seg)
+      if len(alloc) >= requested:
+        assert len(alloc) == requested
+        break
 
     assert len(alloc) == requested
     return alloc
-    
-  @property
-  def free(self):
-    return self.size - self.used
+
+  def commitAllocation(self, ival):
+    """Mark the specified interval as used. Error if overlaps with currently
+       used area."""
+    self._extents = self._extents.union(ival)
 
 class Metadata(object):
   """Represents all metadata of the pad that is saved to disk."""
@@ -189,10 +149,10 @@ class Pad(object):
   """Represents an on-disk pad dir."""
   __metaclass__ = invariant.EnforceInvariant
 
-  def __init__(self, path, create=False):
+  def __init__(self, path, create=False, fsck=False):
     """Opens a pad at the specified path. If create is True, will initialize
        the pad if it does not exist."""
-    s._uncomitted = 0
+    self._uncomitted = 0
     if not os.path.exists(path):
       if create:
         for subdir in "", "incoming", "current", "spent":
@@ -213,7 +173,6 @@ class Pad(object):
       self.metadata.compatability, self.metadata.version = \
           map(int, open("%s/VERSION" % path).read().split("\n"))
     except:
-      raise
       raise InvalidPad("Version missing or corrupt")
 
     # Make sure the pad is compatable
@@ -221,35 +180,61 @@ class Pad(object):
       raise InvalidPad("Pad is protocol %i but I only understand up to %i" % \
                        (self.metadata.compatability, COMPAT))
 
-    # Verify directory structure 
+    # Verify directory structure and check for duplicate filenames.
+    fn = []
     for subdir in "incoming", "current", "spent":
       if not os.path.exists("%s/%s" % (path, subdir)):
         raise InvalidPad("Directory structure bad")
+      fn.extend(os.listdir("%s/%s" % (path, subdir)))
+    if len(fn) != len(set(fn)):
+      raise InvalidPad("Duplicate pad filenames detected. Filenames must be unique.")
 
     # Check for pads that are in metadata but not on-disk. If they
     # have any used space, their absence is an error but if not, they may
     # be stragglers left over from an interrupted "claiming" of a new
     # pad file, and we can savely remove their metadata.
+    # TODO: Verify size during fsck
     for i in reversed(range(len(self.metadata.current))):
       entry = self.metadata.current[i]
+      assert entry.subdir == "current"
       if not os.path.exists("%s/current/%s" % (path, entry.filename)):
-        if entry.used == 0:
+        if entry.used in (0, entry.size):
+          if not fsck:
+            raise PadDirty("Unused or consumed pad %s found in current. This is probably ok." % entry.filename)
+          del self.metadata.current[i]
+        elif os.path.exists("%s/incoming/%s" % (path, entry.filename)):
+          if not fsck:
+            raise PadDirty("Entry %s found in incoming but expected in current." % entry.filename)
+          entry.subdir = "incoming"
+        elif os.path.exists("%s/spent/%s" % (path, entry.filename)):
+          if not fsck:
+            raise PadDirty("Pad found in spent but was not marked as used up in metadata.")
           del self.metadata.current[i]
         else:
           raise InvalidPad("Missing non-unused padfile %s" % entry.filename)
 
     # Verify the size of all pads that are in use
-    for entry in self.metadata.current:
+    for i in reversed(range(len(self.metadata.current))):
+      entry = self.metadata.current[i]
       actual = os.stat("%s/current/%s" % (path, entry.filename))
       if entry.size != actual:
-        raise InvalidPad("Current pad file %s changed size from %i to %i" % \
-                         (entry.filename, entry.size, actual))
+        if not fsck:
+          raise InvalidPad("Current pad file %s changed size from %i to %i" % \
+                           (entry.filename, entry.size, actual))
+        # Can't trust it anymore. Mark it as spent but don't delete so can at least
+        # try to decrypt later.
+        os.rename(entry.path, "%s/spent/%s" % (path, entry.filename))
+        del self.metadata.current[i]
 
     # Save the path
     self.path = path
 
+    # Flush metadata if we were fscking
+    if fsck:
+      self.flush()
+
   def _checkInvariant(self):
-    assert s._uncomitted in (0, 1)
+    assert self._uncomitted in (0, 1)
 
   def flush(self):
     """Flush the pad's current state to disk but do not close it. This will
@@ -259,6 +244,7 @@ class Pad(object):
     # Save old as a backup in case the dump fails. Routines that move the
     # actual files around should be able to tolerate reverting to these if
     # flush/close raises an exception.
+    #TODO write recovery code and inttest it
     os.rename("%s/metadata.pck" % self.path, "%s/metadata.bkp" % self.path)
     os.rename("%s/VERSION" % self.path, "%s/VERSION.bkp" % self.path)
     cPickle.dump(self.metadata, open("%s/metadata.pck" % self.path, 'w'), -1)
@@ -266,9 +252,10 @@ class Pad(object):
 
   def getAllocation(self, requested):
     """Requests the pad to allocate requested bytes of pad. Returns an
-       Allocation or raises OutOfPad if there is no pad left. This can claim
-       pads in incoming (and will move them and sync the metadata), but
-       will not itself mark pads used."""
+       Allocation or raises OutOfPad if there is no pad left. This should
+       not modify the state of the pad in any way except to mark that there
+       is an uncommitted allocation pending. This is done to force serialization
+       of pad claiming to prevent accidental reuse of the same key material."""
     # Look through the files we are already using first.
     current_free = sum((pad.free for pad in self.metadata.current))
 
@@ -279,6 +266,7 @@ class Pad(object):
       new_pads = [(fn, os.stat("%s/incoming/%s" % (self.path, fn)).st_size) \
                   for fn in os.listdir("%s/incoming" % (self.path))]
       new_pads.sort(lambda (pad, size): size)
+      last_need = -1
       for last_need, (pad, size) in enumerate(new_pads):
         can_get += size
         if can_get + current_free >= requested:
@@ -287,46 +275,47 @@ class Pad(object):
         raise OutOfPad("Can't allocate %i bytes; %i bytes available" \
                        % (requested, can_get))
 
-      # Move the pads we need into current. We add all the new metadata
-      # first and flush to disk before moving files, since init code
-      # will silently remove metadata which have missing files and
-      # no used sections.
-      new_files = [File(pad, size) for (pad, size) in new_pads[:last_need + 1]]
-      self.metadata.current.extend(new_files)
-      
-      # Write out the metadata
-      self.flush()
 
-      # Move the files
-      for f in new_files:
-        #TODO: os.rename is not guaranteed to be atomic on Windows iirc. Should
-        # look into this if I want to support windows later.
-        os.rename("%s/incoming/%s" % (self.path, f.filename), \
-                  "%s/current/%s" % (self.path, f.filename))
+    new_files = [File(pad, size, "incoming") \
+                 for (pad, size) in new_pads[:last_need + 1]]
 
     # There is now enough space to actually allocate in current.
     needed = requested
     allocation = Allocation()
-    for pad in self.metadata.current:
+    for pad in self.metadata.current + new_files:
       print "Allocating %i on %s" % (min(needed - len(allocation), pad.free), pad)
       newb = pad.getAllocation(min(needed - len(allocation), pad.free))
       allocation.union_update(newb)
 
     assert len(allocation) == requested
-    s._uncomitted += 1
+    self._uncomitted += 1
     return allocation
 
   def discardUncommitted(self):
     """Releases any outstanding allocations."""
-    assert self._uncomitted == 1
     self._uncomitted = 0
   
   def commitAllocation(self, alloc):
-    """Commits the use of an allocation, and flushes the updated pad to disk"""
+    """Commits the use of an allocation, and writes the updated metadata
+       and any file moves to disk."""
     assert self._uncomitted == 1
-    self._uncomitted = 0
-    #TODO write
-  
+    self.discardUncommitted()
+
+    # Move any files in incoming
+    for (ival, padfile) in alloc.iterFiles():
+      if padfile not in self.metadata.current:
+        assert padfile.subdir == "incoming"
+        os.rename(padfile.path, "%s/current/%s" % (path, padfile.filename))
+        padfile.subdir = "current"
+
+      # Mark used extents as used in file, and move to spent if necessary
+      padfile.markIntervalAsUsed(ival)
+      if padfile.free == 0:
+        os.rename(padfile.path, "%s/spent/%s" % (path, padfile.filename))
+      
+    # Write out the metadata
+    self.flush()
+
   @property
   def uncommitted(self):
     return self._uncomitted
