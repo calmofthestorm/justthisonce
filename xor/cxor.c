4e06389bca63c05d7e753317fd3e5aca74140b0e
Alex Roper <aroper@umich.edu>
2013-07-23 14:31:50 -0400

    Added Python implementation of xor using the same api.

--- xor/cxor.c
+++ xor/cxor.c
@@ -25,7 +25,7 @@ int execute_open_input(XorWorkUnit* work, int index, const char* filename) {
   }
 
   if (filename) {
-    if (!(work->inputs[index] = fopen(filename, "r"))) {
+    if (!(work->inputs[index] = fopen(filename, "rb"))) {
       execute_cleanup(work);
       /* Failed to open file. */
       return -1;
@@ -48,7 +48,7 @@ int execute_open_output(XorWorkUnit* work, const char* filename) {
   }
 
   if (filename) {
-    if (!(work->output = fopen(filename, "a"))) {
+    if (!(work->output = fopen(filename, "ab"))) {
       execute_cleanup(work);
       return -1;
     }
@@ -70,29 +70,33 @@ int execute_seek_input(XorWorkUnit* work, int index, size_t pos) {
 }
 
 int execute_xor(XorWorkUnit* work, size_t length) {
-  size_t size = length < BUFFER_LENGTH ? length : BUFFER_LENGTH;
-  if (!work->inputs[0] || !work->inputs[1] || !work->output ||
-      fread(work->buf[0], 1, size, work->inputs[0]) != size ||
-      fread(work->buf[1], 1, size, work->inputs[1]) != size) {
-    execute_cleanup(work);
-    return -1;
-  }
+  while (length > 0) {
+    size_t size = length < BUFFER_LENGTH ? length : BUFFER_LENGTH;
+    length -= size;
+    if (!work->inputs[0] || !work->inputs[1] || !work->output ||
+        fread(work->buf[0], 1, size, work->inputs[0]) != size ||
+        fread(work->buf[1], 1, size, work->inputs[1]) != size) {
+      execute_cleanup(work);
+      return -1;
+    }
 
-  /* Perform the encryption. Do as much as possible with longs. */
-  for (int i = 0; i < length / sizeof(long); ++i) {
-    /* TODO: Consider using intrinsics if the compiler supports them. */
-    ((long*)work->buf[0])[i] ^= ((long*)work->buf[1])[i];
-  }
+    /* Perform the encryption. Do as much as possible with machine words.
+     * We don't care about endianness because they're read and written in
+     * same order. */
+    for (int i = 0; i < size / sizeof(size_t); ++i) {
+      /* TODO: Consider using intrinsics if the compiler supports them. */
+      ((size_t*)work->buf[0])[i] ^= ((size_t*)work->buf[1])[i];
+    }
 
-  /* Finish off any partial words that are not a multiple. */
-  for (int i = length - (length % sizeof(long)); i < length; ++i) {
-    work->buf[0][i] ^= work->buf[1][i];
-  }
+    /* Finish off any partial words that are not a multiple. */
+    for (int i = size - (size % sizeof(size_t)); i < size ; ++i) {
+      work->buf[0][i] ^= work->buf[1][i];
+    }
 
-  if (fwrite(work->buf[0], 1, size, work->output) != size) {
-    execute_cleanup(work);
-    return -1;
-  }
+    if (fwrite(work->buf[0], 1, size, work->output) != size) {
+      execute_cleanup(work);
+      return -1;
+    }
 
   return 0;
 }
--- xor/xor.py
+++ xor/xor.py
@@ -1,8 +1,14 @@
 import ctypes
 import cxorlib
+import sys
+import os
 
-# TODO: this needs to be relative to application dir.
-_xorlib=ctypes.cdll.LoadLibrary("/home/alexr/projects/justthisonce/xor/cxor.so")
+try:
+  # TODO: this needs to be relative to application dir.
+  _xorlib=ctypes.cdll.LoadLibrary("/home/alexr/projects/justthisonce/xor/cxor.so")
+except Exception:
+  sys.stderr.write("Error loading C XOR library; falling back to (slow) Python.")
+  _xorlib = None
 
 class Error(Exception):
   pass
@@ -17,30 +23,96 @@ def execute(fxn, *args):
   if fxn(*args) != 0:
     raise CXORError()
 
-def xorAllocation(alloc, infile, outfile):
+class PyXOR(object):
+  """Pure Python implementation of the CXOR interface. Used both for testing and
+     as an option should the C implementation be unavailable."""
+  BUFFER_LENGTH = 4 * 1024 * 1024
+  class PyXORWorkUnit(object):
+    def __init__(self):
+      self.inputs = [None, None]
+      self.output = None
+
+  @staticmethod
+  def execute_open_input(work, index, filename):
+    if work.inputs[index] and work.inputs[index] != sys.stdin:
+      work.inputs[index].close()
+    if filename:
+      work.inputs[index] = open(filename, "rb")
+    else if work.inputs[0 if index == 1 else 1] != sys.stdin:
+      work.inputs[index] = sys.stdin
+    else:
+      execute_cleanup(work)
+      return -1
+    return 0
+
+  @staticmethod
+  def execute_open_output(work, filename):
+    if work.output and work.output != sys.stdout:
+      work.output.close()
+    if filename:
+      work.output = open(filename, "ab")
+    else:
+      work.output = sys.stdout
+    return 0
+
+  @staticmethod
+  def execute_seek_input(work, index, pos):
+    if !work.inputs[index] or work.inputs[index] == sys.stdin:
+      execute_cleanup(work)
+      return -1
+    work.inputs[index].seek(ps, os.SEEK_SET)
+
+  @staticmethod
+  def execute_xor(work, length):
+    while length > 0:
+      size = min(length, BUFFER_LENGTH)
+      length -= size
+
+      data = zip(work.inputs[i].read(size) for i in xrange(2))
+      if len(data) != size:
+        execute_cleanup(work)
+        return -1
+      work.output.write(chr(ord(a) ^ ord(b)) for (a, b) in data)
+
+  @staticmethod
+  def execute_cleanup(work):
+    for fd in [work.output] + work.inputs:
+      if fd not in (None, sys.stdin, sys.stdout):
+        fd.close()
+    work.output = work.inputs[0] = work.inputs[1] = None
+
+def xorAllocation(alloc, infile, outfile, impl="C"):
   """Given an allocation and an input file, xor the allocation with the input
      file and *append* the result to the specified output file. infile and/or
      outfile should be None to indicate stdin/stdout."""
+  if impl == "Python" or _xorlib is None:
+    xor = PyXOR
+    work = PyXOR.PyXORWorkUnit()
+  elif impl == "C":
+    xor = _xorlib 
+    work = cxorlib.XorWorkUnit()
+  else:
+    raise Error("Unknown encryption provider: %s" % impl)
+
   # Check the allocation is the correct length.
   infile_size = os.stat(infile).st_size
   if infile_size != len(alloc):
     raise AllocationSizeMismatch(os.stat(infile), len(alloc))
 
-  work = cxorlib.XorWorkUnit()
   work.output = None
   work.inputs[0] = None
   work.inputs[1] = None
 
   try:
-    execute(cxor.execute_open_input, work, 1, infile)
-    execute(cxor.execute_open_output, work, outfile)
+    execute(xor.execute_open_input, work, 1, infile)
+    execute(xor.execute_open_output, work, outfile)
 
     # Encrypt the allocation one interval at a time.
     for (pad_interval, pad_file) in alloc.iterValues():
-      execute(cxor.execute_open_input, work, 0, pad_file)
+      execute(xor.execute_open_input, work, 0, pad_file)
       for (start, length) in pad_interval.toAtoms():
-        execute(cxor.execute_seek_input, work, 0, start)
-        execute(cxor.execute_xor, work, length)
-    execute(cxor.execute_cleanup, work)
+        execute(xor.execute_seek_input, work, 0, start)
+        execute(xor.execute_xor, work, length)
+    execute(xor.execute_cleanup, work)
   except AssertionError:
     raise CXORError()
